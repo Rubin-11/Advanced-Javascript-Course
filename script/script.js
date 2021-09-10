29b8d7b617a20175284933fb9c23bdc19d019ac6
Sergey Misharin <misharin_sergey-work@mail.ru>
2021-09-11 01:51:02 +0300

    Homework for lesson 2

--- script/script.js
+++ script/script.js
@@ -1,19 +1,57 @@
-const goods = [
-    {title: 'Shirt', price: 150},
-    {title: 'Socks', price: 50},
-    {title: 'Jacket', price: 350},
-    {title: 'Shoes', price: 250},
-];
-
-const renderGoodsItem = (title = 'Название товара', price = "Цена товара") => `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-
-const renderGoodsList = list => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    let array = goodsList.join('');
-    document.querySelector('.goods-list').innerHTML = array;
-    // Свойство интерфейса innerHTML устанавливает или получает HTML разметку дочерних элементов.
-    // Когда мы передовали массив goodsList, innerHTML выводил и запятые.
-    // После преобразования массива в строку с помошью метода join('') и убрав запятые,
-    // передаем чистый текст.
+class GoodsList { // список товаров
+    constructor() {
+        this.goods = [];
+    }
+    fetchGoods() { // заполнение списка товара.
+        this.goods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 350 },
+            { title: 'Shoes', price: 250 },
+        ];
+    }
+    render() { // вывод списка товаров
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
+        }
+    sumPriceGoods() { // сумарная стоимось всех товаров
+        let sum = 0;
+        this.goods.forEach(goods => {
+            sum += goods.price;
+        });
+        alert(`сумарная стоимось всех товаров ${sum}`);
+    }
 }
-renderGoodsList(goods);
+
+class GoodsItem { // товар
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    render() { // разметка HTML
+        return `<div class="goods-item">
+        <img src = "img/img.png" width = 100px>
+        <h3>${this.title}</h3>
+        <p>${this.price}</p>
+        <button>Купить</button>
+        </div>`;
+    }
+}
+
+class BasketGoods { // корзина товаров
+    // метод очистки корзины
+    // метод купить все
+}
+
+class BasketElement { // элемент корзины
+    // метод подсчета количество товара одного вида
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+list.sumPriceGoods();
