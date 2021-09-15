49fa6574487518b074f814ffff20a4478336551d
Sergey Misharin <misharin_sergey-work@mail.ru>
2021-09-15 19:11:17 +0300

    Homework for lesson 3

--- script/script.js
+++ script/script.js
@@ -1,57 +1,179 @@
-class GoodsList { // список товаров
-    constructor() {
-        this.goods = [];
+const API_URL =
+  "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
+
+class GoodsList {
+  // список товаров
+  constructor(basket) {
+    this.basket = basket;
+    this.goods = [];
+    this.goodsObjs = [];
+    this.getGoods().then((data) => {
+      this.goods = data;
+      this.render();
+    });
+    this.makeEvents();
+  }
+  getGoods() {
+    // заполнение списка товара.
+    return fetch(`${API_URL}/catalogData.json`)
+      .then((response) => response.json())
+      .catch((err) => console.log(err));
+  }
+  render() {
+    const block = document.querySelector(".goods-list");
+    for (let product of this.goods) {
+      console.log(this.constructor.name);
+
+      let obj = new GoodsItem(product);
+
+      console.log(obj);
+      this.goodsObjs.push(obj);
+      block.insertAdjacentHTML("beforeend", obj.render());
     }
-    fetchGoods() { // заполнение списка товара.
-        this.goods = [
-            { title: 'Shirt', price: 150 },
-            { title: 'Socks', price: 50 },
-            { title: 'Jacket', price: 350 },
-            { title: 'Shoes', price: 250 },
-        ];
-    }
-    render() { // вывод списка товаров
-        let listHtml = '';
-        this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.title, good.price);
-            listHtml += goodItem.render();
-        });
-        document.querySelector('.goods-list').innerHTML = listHtml;
-        }
-    sumPriceGoods() { // сумарная стоимось всех товаров
-        let sum = 0;
-        this.goods.forEach(goods => {
-            sum += goods.price;
-        });
-        alert(`сумарная стоимось всех товаров ${sum}`);
-    }
+  }
+  sumPriceGoods() {
+    // сумарная стоимось всех товаров
+    let sum = 0;
+    this.goods.forEach((goods) => {
+      sum += goods.price;
+    });
+    return sum;
+  }
+  makeEvents() {
+    document.querySelector(".goods-list").addEventListener("click", (e) => {
+      if (e.target.classList.contains("buy")) {
+        this.basket.addProduct(e.target);
+      }
+    });
+  }
 }
 
-class GoodsItem { // товар
-    constructor(title, price) {
-        this.title = title;
-        this.price = price;
-    }
-    render() { // разметка HTML
-        return `<div class="goods-item">
+class GoodsItem {
+  // товар
+  constructor(e) {
+    this.id = e.id_product;
+    this.product_name = e.product_name;
+    this.price = e.price;
+  }
+  render() {
+    // разметка HTML
+    return `<div class="goods-item">
         <img src = "img/img.png" width = 100px>
-        <h3>${this.title}</h3>
+        <h3>${this.product_name}</h3>
         <p>${this.price}</p>
-        <button>Купить</button>
+        <button class="buy" 
+        data-id="${this.id}"
+        data-name="${this.product_name}"
+        data-price="${this.price}">Купить</button>
         </div>`;
+  }
+}
+
+class BasketGoods {
+  // корзина товаров
+  constructor() {
+    this.goods = [];
+    this.goodsObjs = [];
+    this.getBasket().then((data) => {
+      this.goods = data.contents;
+      this.render();
+    });
+    this.makeEvents();
+  }
+  getBasket() {
+    // заполнение списка корзины.
+    return fetch(`${API_URL}/getBasket.json`)
+      .then((response) => response.json())
+      .catch((err) => console.log(err));
+  }
+  addProduct(e) {
+    let productId = +e.dataset["id"];
+    let find = this.goodsObjs.find((product) => product.id === productId);
+    if (find) {
+      find.quantity++;
+      let block = document.querySelector(`.basket-item[data-id="${find.id}"]`);
+      block.querySelector(
+        ".product-quantity"
+      ).textContent = `Количество: ${find.quantity}`;
+      block.querySelector(".product-price").textContent = `${
+        find.quantity * find.price
+      } ₽`;
+    } else {
+      let product = {
+        id_product: productId,
+        price: +e.dataset["price"],
+        product_name: e.dataset["name"],
+        quantity: 1
+      };
+      this.goods = [product];
+      this.render();
+    }
+  }
+  removeProduct(e) {
+    let productId = +e.dataset["id"];
+    let find = this.goodsObjs.find((product) => product.id === productId);
+    if (find.quantity > 1) {
+      find.quantity--;
+      let block = document.querySelector(`.basket-item[data-id="${find.id}"]`);
+      block.querySelector(
+        ".product-quantity"
+      ).textContent = `Количество: ${find.quantity}`;
+      block.querySelector(".product-price").textContent = `${
+        find.quantity * find.price
+      } ₽`;
+    } else {
+      this.goodsObjs.splice(this.goodsObjs.indexOf(find), 1);
+      document.querySelector(`.basket-item[data-id="${productId}"]`).remove();
+    }
+  }
+  makeEvents() {
+    document.querySelector(".basket-button").addEventListener("click", () => {
+      document.querySelector(".basket").classList.toggle("invisible");
+    });
+    document.querySelector(".basket").addEventListener("click", (e) => {
+      if (e.target.classList.contains("del-basket-item")) {
+        this.removeProduct(e.target);
+      }
+    });
+  }
+  render() {
+    const block = document.querySelector(".basket");
+    for (let product of this.goods) {
+        console.log(this.constructor.name);
+
+        let obj = new BasketElement(product);
+
+        console.log(obj);
+        this.goodsObjs.push(obj);
+        block.insertAdjacentHTML("beforeend", obj.render());
+    }
     }
 }
 
-class BasketGoods { // корзина товаров
-    // метод очистки корзины
-    // метод купить все
+class BasketElement {
+  // элемент корзины
+    constructor(e) {
+    this.id = e.id_product;
+    this.product_name = e.product_name;
+    this.price = e.price;
+    this.quantity = e.quantity;
+    }
+    render() {
+    return `<div class="basket-item" data-id="${this.id}">
+    <div>
+        <img src="img/img.png">
+        <div class="">
+            <p class="">${this.product_name}</p>
+            <p class="product-quantity">Количество: ${this.quantity}</p>
+            <p class="">${this.price} за ед.</p>
+        </div>
+        </div>
+        <div class="">
+        <p class="product-price">${this.quantity * this.price} ₽</p>
+        <button class="del-basket-item" data-id="${this.id}">&times;</button>
+    </div>
+    </div>`;
+    }
 }
 
-class BasketElement { // элемент корзины
-    // метод подсчета количество товара одного вида
-}
-
-const list = new GoodsList();
-list.fetchGoods();
-list.render();
-list.sumPriceGoods();
+new GoodsList(new BasketGoods());
